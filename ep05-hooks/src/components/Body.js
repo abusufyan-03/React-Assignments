0595135ade7f0b16ce12a49ec01174b9760f677e
sufiyan <sufiyan.manuu@gmail.com>
2026-03-31 14:12:32 +0530

    Build top rated restaurant filter feature

--- ep05-hooks/src/components/Body.js
+++ ep05-hooks/src/components/Body.js
@@ -1,15 +1,24 @@
 import resList from '../utils/resList.js'
 import RestaurantCards from './RestaurantCards.js'
+import { useState } from 'react';
 const Body = () => {
+    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
     return (
         <main className="main">
             <section>
                 <div className="search-container">
                     <input className="search-bar" type="text" placeholder="search..." />
                     <button className="search-btn">Search</button>
+                    <button className='filter-btn' 
+                    onClick={()=>{
+                         /* Filter logic */
+                         const filterRestaurant = listOfRestaurant.filter((res)=> res.data.avgRating>4)
+                         setListOfRestaurant(filterRestaurant);
+                    }}>
+                    Top rated Restaurant</button>
                 </div>
                 <div className="restaurant-container">
-                    {resList.map((restaurant)=><RestaurantCards key={restaurant.data.id} resData={restaurant}/>)}
+                    {listOfRestaurant.map((restaurant)=><RestaurantCards key={restaurant.data.id} resData={restaurant}/>)}
                 </div>
             </section>
         </main>
