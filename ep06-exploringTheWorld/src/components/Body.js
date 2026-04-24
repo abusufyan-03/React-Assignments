f5033bf06067d2e91e7ccdb93c234d7f89e93096
sufiyan <sufiyan.manuu@gmail.com>
2026-04-24 19:02:16 +0530

    Build Filtered Restaurant logic

--- ep06-exploringTheWorld/src/components/Body.js
+++ ep06-exploringTheWorld/src/components/Body.js
@@ -1,16 +1,24 @@
 import resList from "../utils/resList";
 import RestaurantCards from "./RestaurantCards";
+import { useState } from "react";
 const Body = () => {
+    const [restaurantList, setRestaurantList] = useState(resList)
     return(
         <main className="main">
             <section>
                 <div className="search-container">
                     <input className="search-bar" type="search" placeholder="search..."/>
                     <button className="search-btn btn">Search</button>
-                    <button className="filter-btn btn">Top Rated Restaurant</button>
+                    <button className="filter-btn btn"
+                    onClick={()=>{
+                        const filterRestaurant = restaurantList.filter((res)=> res.data.avgRating > 4 )
+                        setRestaurantList(filterRestaurant);
+                    }}
+                    
+                    >Top Rated Restaurant</button>
                 </div>
                 <div className="restaurant-container">
-                    {resList.map((restaurant, index)=><RestaurantCards key={restaurant.data.id} resData={restaurant}/>)}
+                    {restaurantList.map((restaurant, index)=><RestaurantCards key={restaurant.data.id} resData={restaurant}/>)}
                 </div>
             </section>
         </main>
