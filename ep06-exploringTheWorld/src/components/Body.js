b1a23b0565de7978df711f8591a8f7489b43c7f2
sufiyan <sufiyan.manuu@gmail.com>
2026-04-25 16:15:50 +0530

    Fix Search functionality bug by creating another state variable: instead of filter restaurant from filtered restaurant we are having a copy of restaurant which has all restaurant and filtering from it only

--- ep06-exploringTheWorld/src/components/Body.js
+++ ep06-exploringTheWorld/src/components/Body.js
@@ -5,65 +5,71 @@ import Shimmer from "./Shimmer";
 const Body = () => {
     const [restaurantList, setRestaurantList] = useState([])
     const [searchText, setSearchText] = useState('');
+    const [filterRestaurant, setFilterRestaurant] = useState([]);
 
-    useEffect(() =>{
-    // console.log("useEffect get called!")
-    fetchData();
-}, []);
+    useEffect(() => {
+        // console.log("useEffect get called!")
+        fetchData();
+    }, []);
 
-const fetchData = async ()=> {
-    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING');
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.624480699999999&page_type=DESKTOP_WEB_LISTING');
 
-    const json = await data.json();
-    console.log(json);
+        const json = await data.json();
+        console.log(json);
 
-    setRestaurantList(json?.data?.cards[2].card.card.gridElements.infoWithStyle.restaurants);
-    console.log(json?.data?.cards[2].card.card.gridElements.infoWithStyle.restaurants)
-}
+        setRestaurantList(json?.data?.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setFilterRestaurant(json?.data?.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        console.log(json?.data?.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    }
 
-/* *Conditional rendering */
-// if(restaurantList.length == 0){
-//     return(
-//         <Shimmer/>
-//     )
-// }
-// console.log("Body called")
-    return restaurantList.length==0? <Shimmer/>: (
+    /* *Conditional rendering */
+    // if(restaurantList.length == 0){
+    //     return(
+    //         <Shimmer/>
+    //     )
+    // }
+    // console.log("Body called")
+
+    const handleSearch = () => {
+        let searchedRestaurant = restaurantList.filter((res) => {
+            return res.info.name.toLowerCase().includes(searchText.toLowerCase())
+        })
+        setFilterRestaurant(searchedRestaurant)
+    }
+    return restaurantList.length == 0 ? <Shimmer /> : (
         <main className="main">
             <section>
                 <div className="search-container">
-                    <input 
-                    className="search-bar"
-                    id="search"
-                    onChange={(e)=>{
-                        setSearchText(e.target.value);
-                        console.log(searchText)
-                    }}
-                    type="text" 
-                    placeholder="search..."/>
+                    <input
+                        className="search-bar"
+                        id="search"
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                            console.log(searchText)
+                        }}
+                         onKeyPress={(e)=>{
+                            if(e.key === 'Enter'){
+                                handleSearch();
+                            }
+                        }}
+                        type="text"
+                        placeholder="search..." />
                     <button className="search-btn btn"
                     onClick={()=>{
-                        let searchedRestaurant = restaurantList.filter((res)=>{
-                            return res.info.name.toLowerCase().includes(searchText.toLowerCase())
-                        })
-                        setRestaurantList(searchedRestaurant)
-
-                        console.log(searchText)
-                        console.log(searchedRestaurant)
+                        handleSearch();
                     }}
                     >Search</button>
                     <button className="filter-btn btn"
-                    onClick={()=>{
-                        const filterRestaurant = restaurantList.filter((res)=> res.info.avgRating > 4 )
-                        setRestaurantList(filterRestaurant);
-                        console.log(filterRestaurant)
-                        console.log('filter btn clicked')
-                    }}
-                    
+                        onClick={() => {
+                            const filterRestaurant = restaurantList.filter((res) => res.info.avgRating > 4.5)
+                            setFilterRestaurant(filterRestaurant);
+                        }}
+
                     >Top Rated Restaurant</button>
                 </div>
                 <div className="restaurant-container">
-                    {restaurantList.map((restaurant, index)=><RestaurantCards key={index} resData={restaurant}/>)}
+                    {filterRestaurant.map((restaurant, index) => <RestaurantCards key={index} resData={restaurant} />)}
                 </div>
             </section>
         </main>
