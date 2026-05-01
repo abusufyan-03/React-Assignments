ef02def4992995d51d8b6c36321f51b04e418142
sufiyan <sufiyan.manuu@gmail.com>
2026-05-01 21:35:29 +0530

    Played with console logs to find out correct order of their execution and also created interval inside componentDidMo unt, which makes this script to run even after going to another page, to fix that we used componentWillUnmount and clearIntervalgit

--- ep08-letsGetClassy/src/components/About.js
+++ ep08-letsGetClassy/src/components/About.js
@@ -1,8 +1,18 @@
 import React from "react";
 import UserClass from "./UserClass";
-
+import User from "./user";
 class About extends React.Component{
+    constructor(props){
+        super(props)
+
+        console.log("Parent construtor")
+    }
+
+    componentDidMount(){
+        console.log("Parent componentDidMount")
+    }
     render(){
+        console.log("parent render")
         return(
         <>
         <h1>About US Page</h1>
--- ep08-letsGetClassy/src/components/UserClass.js
+++ ep08-letsGetClassy/src/components/UserClass.js
@@ -9,8 +9,28 @@ class UserClass extends React.Component{
             count: 1,
             count2: 2
         }
+
+        console.log(this.props.name + " child Constructor")
+    }
+
+    componentDidMount(){
+        console.log("Child componentDidMount")
+        /* this.timer = setInterval(()=>{
+            console.log("interval running");
+        }, 1000) */
+    }
+
+    componentDidUpdate(){
+        console.log("component did update")
+    }
+
+    componentWillUnmount(){
+
+        console.log("component will unmount")
+        // clearInterval(this.timer)
     }
     render(){
+        console.log(this.props.name + "child render")
         let { count, count2 } = this.state
         return(
             <>
