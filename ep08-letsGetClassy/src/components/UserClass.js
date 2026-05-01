3230cf3d73c12b9a4c02e048fad925c2f0d72b4f
sufiyan <sufiyan.manuu@gmail.com>
2026-05-01 19:28:03 +0530

    created multiple state varaible and also updated it

--- ep08-letsGetClassy/src/components/UserClass.js
+++ ep08-letsGetClassy/src/components/UserClass.js
@@ -4,13 +4,27 @@ class UserClass extends React.Component{
     constructor(props){
         super(props)
         console.log(props)
+
+        this.state = {
+            count: 1,
+            count2: 2
+        }
     }
     render(){
+        let { count, count2 } = this.state
         return(
             <>
             <div className="user-card">
                 <h1>This is Class Base Component</h1>
                 <p>Name: {this.props.name} </p>
+                <p>Count: {count} </p>
+                <p>Count: {count2} </p>
+                <button onClick={()=>{
+                    this.setState({
+                        count: count + 1,
+                        count2: count2 + 1
+                    })
+                }}>increment</button>
             </div>
             </>
         )
